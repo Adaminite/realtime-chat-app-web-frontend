6bf2a9ca1c78bf2210d1c7d91c6e044378f66e4c
Omar Niazi <niazi.omark@gmail.com>
2023-10-31 08:29:56 -0400

    Set up barebones client to test websocket server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChannelsComponent } from './channels/channels.component';
+import { ChannelComponent } from './channel/channel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChannelsComponent,
+    ChannelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/channels/channels.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder } from '@angular/forms';
+import { webSocket } from 'rxjs/webSocket';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './channels.component.html',
+  styleUrls: ['./channels.component.css']
+})
+export class ChannelsComponent implements OnInit {
+  messageForm = this.formBuilder.group({
+    message: new FormControl('')
+  });
+
+  messages: Array<string> = [];
+  ws = webSocket("ws://localhost:3000");
+
+  channelForm = this.formBuilder.group({
+    channelName: new FormControl('')
+  });
+
+  constructor(private formBuilder: FormBuilder){}
+  
+  ngOnInit() : void {
+    
+    this.ws.subscribe({
+      next: (value: any) => {
+        console.log(value);
+        this.messages.push(String(value["message"]));
+      },
+      error: (err) => {console.log("Error:" + err)},
+      complete: () => {console.log("Connection closed")}
+    });
+    
+
+    /*
+    this.ws.addEventListener('message', (event: MessageEvent) => {
+      console.log(event);
+    });
+    */
+  }
+
+  async createRoom() : Promise<void> {
+    const channelName : string | null | undefined = this.channelForm.value["channelName"];
+    console.log(JSON.stringify({"channelName": channelName}))
+    const response = await fetch('http://localhost:3000/channels/create', {
+      method: "POST",
+      mode: 'cors',
+      headers : {
+        "Content-Type": "application/json"
+      },
+      body : JSON.stringify({channelName})
+    });
+
+    const json = await response.json();
+    console.log(json);
+  }
+  /*
+  sendMessage(){
+
+  }
+ */ 
+  onSubmit() : void {
+    console.log(this.messageForm);
+    const message: string | null | undefined = this.messageForm.value["message"];
+    console.log("Message: " + message);
+    //this.messages.push();
+    this.ws.next({'message': message});
+    //this.ws.send(String(message));
+  }
+  
+}
